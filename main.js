8a468e3739e1168649e3088663cb8bf65ffaf5d4
Ping <dev.bastianabel@gmail.com>
2024-12-07 23:57:19 +0100

    search function added and overlay with rendering finished

--- main.js
+++ main.js
@@ -1,9 +1,16 @@
-const API_URL = "https://pokeapi.co/api/v2/";
-const BASE_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/";
-const CONTAINER_REF = document.getElementById("card-container");
+const API_ALL_URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+const IMG_BASE_URL = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/";
+const CARD_CONTAINER_REF = document.getElementById("card-container");
+const OVERLAY_REF = document.getElementById("detailed-info-container");
+const RENDER_MORE_REF= document.getElementById("render-more-container");
+const SEARCHBAR_REF = document.getElementById("search-bar");
 
-let loadedPokemon = "";
-let numToLoad = 3;
+let allPokemon = [];
+let searchedPokemon = [];
+let numToLoad = 10;
+let loadedPokemon = 0;
+let searchbarActive = false;
+let searchfieldInput = "";
 
 async function init() {
     await getPokemon();
@@ -11,27 +18,27 @@ async function init() {
 }
 
 async function getPokemon() {
-    let response = await fetch(API_URL + "pokemon?limit=" + numToLoad + "&offset=0" + ".json")
-    loadedPokemon = await response.json();
-    loadedPokemon = loadedPokemon["results"];
+    const response = await fetch(API_ALL_URL)
+    const data = await response.json();
+    allPokemon = data.results;
 }
 
 async function setCurrentPokemon(indexOfPokemon) {
-    currentPokemon = await fetch(loadedPokemon[indexOfPokemon]["url"])
+    currentPokemon = await fetch(allPokemon[indexOfPokemon]["url"])
         currentPokemon = await currentPokemon.json();
 }
 
 async function renderPokemonCards() {
-    CONTAINER_REF.innerHTML = "";
-    for (let i = 0; i < loadedPokemon.length; i++) {
-        await setCurrentPokemon(i);
-        pokemonName = currentPokemon["name"].charAt(0).toUpperCase() + currentPokemon["name"].slice(1);
-        pokemonId = "#" + currentPokemon["id"];
-        pokemonImgUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/" + currentPokemon["id"] + ".png";
-        pokemonFirstType = currentPokemon["types"][0]["type"]["name"];    
-        pokemonTypeHTML = fillTypeTemplate();
-        CONTAINER_REF.innerHTML += getPokemonCardTemplate(pokemonName, pokemonId, pokemonImgUrl, pokemonFirstType, pokemonTypeHTML)
+    CARD_CONTAINER_REF.innerHTML = "";
+    for (;loadedPokemon < numToLoad; loadedPokemon++) {
+        await setCurrentPokemon(loadedPokemon);
+        CARD_CONTAINER_REF.innerHTML += fillPokemonCardTemplate();
     }
+}
+
+function fillPokemonCardTemplate() {
+    getPokemonInfo();
+    return getPokemonCardTemplate(pokemonName, pokemonId, pokemonTypeHTML, pokemonFirstType);
 }
 
 function fillTypeTemplate() {
@@ -39,41 +46,119 @@ function fillTypeTemplate() {
     type = "";
     for(let j = 0; j < typeList.length; j++) {
         typeName = typeList[j]["type"]["name"];
-        typeImgNr = typeList[j]["type"]["url"].slice(31, 33);
+        typeImgNr = typeList[j]["type"]["url"].slice(31);
         typeImgNr = typeImgNr.replace("/", "")
-        typeImgUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/types/generation-viii/sword-shield/" + typeImgNr + ".png";
+        typeImgUrl = IMG_BASE_URL + "types/generation-viii/sword-shield/" + typeImgNr + ".png";
         type += getPokemonCardTypeTemplate(typeImgUrl, typeName)
     }
     return type;
 }
 
-function openOverlay() {
-    toggleClass("body" ,"overflow-hidden");
-    toggleClass("overlay" ,"d-none");
-    renderOverlay();
+function openOverlay(pokemonId,) {
+    toggleClass("overflow-hidden", "body");
+    toggleClass("d-none", "overlay");
+    renderOverlay(pokemonId);
 }
 
-function toggleClass(elementid, classToToggle) {
-    elementRef = document.getElementById(elementid);
-    elementRef.classList.toggle(classToToggle);
+function toggleClass(classToToggle, ...elementid) {
+    for(let i = 0; i < elementid.length; i++) {
+        elementRef = document.getElementById(elementid[i]);
+        if(elementRef) {
+            elementRef.classList.toggle(classToToggle);    
+        }
+    }
 }
 
-function renderOverlay(pokemonId) {
+async function renderOverlay(pokemonId) {
+    await setCurrentPokemon(pokemonId - 1);
+    await getPokemonInfo();
+    OVERLAY_REF.innerHTML = getOverlayCardTemplate(pokemonName, pokemonId, pokemonTypeHTML, baseExp, height, weight, hpInPercent, attackInPercent, defenseInPercent, specialattackInPercent, specialdefenseInPercent, speedInPercent, evoChainHTML);
+}
 
+async function getPokemonInfo() {
+    pokemonName = currentPokemon["name"].charAt(0).toUpperCase() + currentPokemon["name"].slice(1);
+    pokemonId = currentPokemon["id"];
+    pokemonFirstType = currentPokemon["types"][0]["type"]["name"];    
+    pokemonTypeHTML = fillTypeTemplate();
+    baseExp = currentPokemon["base_experience"];
+    height = currentPokemon["height"] / 10;
+    weight = currentPokemon["weight"] / 10;
+    hpInPercent = Math.round((currentPokemon["stats"][0]["base_stat"] / 255) * 100);
+    attackInPercent = Math.round((currentPokemon["stats"][1]["base_stat"] / 255) * 100);
+    defenseInPercent = Math.round((currentPokemon["stats"][2]["base_stat"] / 255) * 100);
+    specialattackInPercent = Math.round((currentPokemon["stats"][3]["base_stat"] / 255) * 100);
+    specialdefenseInPercent = Math.round((currentPokemon["stats"][4]["base_stat"] / 255) * 100);
+    speedInPercent = Math.round((currentPokemon["stats"][5]["base_stat"] / 255) * 100);
+    evoChainHTML = await getEvolutionChain(pokemonId);
+}
+
+async function getEvolutionChain(pokemonId) {
+    evoJson = await getEvolutionChainJson(pokemonId);
+    evoChainHTML = getEvoChainImgTemplate(1, evoJson["chain"]["species"]["url"].slice(42).replace("/", ""));
+    if(evoJson?.chain?.evolves_to?.[0]) {
+        evoChainHTML += getEvoChainArrowTemplate();
+        evoChainHTML += getEvoChainImgTemplate(2, evoJson["chain"]["evolves_to"][0]["species"]["url"].slice(42).replace("/", ""))
+    }
+    if(evoJson?.chain?.evolves_to?.[0]?.evolves_to?.[0]) {
+        evoChainHTML += getEvoChainArrowTemplate();
+        evoChainHTML += getEvoChainImgTemplate(3, evoJson["chain"]["evolves_to"][0]["evolves_to"][0]["species"]["url"].slice(42).replace("/", ""))
+    }
+    return evoChainHTML
+}
+
+async function getEvolutionChainJson(pokemonId) {
+    evoUrl = await fetch("https://pokeapi.co/api/v2/pokemon-species/" + pokemonId);
+    evoUrl = await evoUrl.json();
+    evoUrl = await fetch(evoUrl["evolution_chain"]["url"]);
+    evoJson = evoUrl.json();
+    return evoJson
 }
 
 function closeOverlay() {
-    toggleClass("body" ,"overflow-hidden");
-    toggleClass("overlay" ,"d-none");
+    toggleClass("overflow-hidden", "body");
+    toggleClass("d-none", "overlay");
 }
 
 function stopPropagation(event) {
     event.stopPropagation();
 }
 
-function test(event) {
-    if(event != undefined){
-        event.preventDefault();
-        console.log(1);
+function searchForPokemon(event) {
+    event?.preventDefault();
+    searchfieldInput = SEARCHBAR_REF.value;
+    filterPokemonList();
+    renderSearchedPokemon();
+}
+
+async function renderSearchedPokemon() {
+    CARD_CONTAINER_REF.innerHTML = "";
+    loadedPokemon = 0;
+    for (;loadedPokemon < numToLoad; loadedPokemon++) {
+        await setCurrentPokemon(searchedPokemon[loadedPokemon]);
+        CARD_CONTAINER_REF.innerHTML += fillPokemonCardTemplate()
+    }
+}
+
+function filterPokemonList() {
+    searchedPokemon = [];
+    for(const pokemon of allPokemon) {
+        if(pokemon.name.includes(searchfieldInput)) {
+            pokemonId = pokemon["url"].slice(34);
+            pokemonId = pokemonId.replace("/", "")    
+            searchedPokemon.push(pokemonId -1)
+        }
+    }
+}
+
+function changeInfoTab(selectedTab, ...deselect) {
+    navToSelect = document.getElementById(selectedTab);
+    tabToSelect = document.getElementById("detailed-info-" + selectedTab)
+    navToSelect.setAttribute("class", "selected");
+    tabToSelect.setAttribute("class", "");
+    for(let i = 0; i < deselect.length; i++) {
+        navToDeselect = document.getElementById(deselect[i])        
+        tabToDeselect = document.getElementById("detailed-info-" + deselect[i])  
+        navToDeselect.setAttribute("class", "");
+        tabToDeselect.setAttribute("class", "d-none");          
     }
 }
--- scripts/template.js
+++ scripts/template.js
@@ -1,25 +1,177 @@
-function getPokemonCardTemplate(pokemonName, pokemonId, pokemonImgUrl, pokemonFirstType, pokemonTypeHTML) {
+function getPokemonCardTemplate(pokemonName, pokemonId, pokemonTypeHTML, pokemonFirstType) {
     return `
-                <div class="pokemon-card" onclick="openOverlay()">
+            <div class="pokemon-card" onclick="openOverlay(${pokemonId})">
                 <div class="card-header">
                     <h2 class="m-0 p-8">${pokemonName}</h2>
-                    <h2 class="m-0 p-8">${pokemonId}</h2>
+                    <h2 class="m-0 p-8">${pokemonId}#</h2>
                 </div>
                 <div class="basic-info">
                     <div class="pokemon-img ${pokemonFirstType}">
-                        <img src="${pokemonImgUrl}"
+                        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId}.png"
                         alt="">
                     </div>
                     <div class="pokemon-types">
                         ${pokemonTypeHTML}
                     </div>
                 </div>
+            </div>
     `
 }
 
 function getPokemonCardTypeTemplate(typeImgUrl, typeName) {
     return `
-                            <img class="p-8" src="${typeImgUrl}"
-                            alt="${typeName}">
+            <img class="p-8" src="${typeImgUrl}"
+            alt="${typeName}">
     `
 }
+
+function getOverlayCardTemplate(pokemonName, pokemonId, pokemonTypeHTML, baseExp, height, weight, hpInPercent, attackInPercent, defenseInPercent, specialattackInPercent, specialdefenseInPercent, speedInPercent, evoChain) {
+    return`
+            <div class="overlay-header p-8"><span>${pokemonName}</span><span>${pokemonId}#<button onclick="closeOverlay()">x</button></span></div>
+            <div class="overlay-img-and-types">
+                <div>
+                    <img id="normal-img" class="overlay-pokemon-img"
+                        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId}.png"
+                        alt="">
+                    <img id="shiny-img" class="overlay-pokemon-img d-none"
+                        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${pokemonId}.png"
+                        alt="">
+                </div>
+                <div class="overlay-type-container">
+                    <div class="toggle-shiny">
+                        <button class="toggle-button" id="toggle-button"
+                            onclick="toggleClass('active', 'toggle-button'), toggleClass('d-none', 'normal-img', 'shiny-img', 'evo-img-1', 'evo-img-1-shiny', 'evo-img-2', 'evo-img-2-shiny', 'evo-img-3', 'evo-img-3-shiny')">
+                            <div class="toggle-circle"></div>
+                        </button>
+                        <span><i class="fa fa-arrow-left"></i> Shiny</span>
+                    </div>
+                    ${pokemonTypeHTML}
+                </div>
+            </div>
+            <nav class="detailed-info-nav">
+                <div><span onclick="changeInfoTab('main', 'stats', 'evo-chain')" id="main" class="selected">Main</span></div>
+                <div><span onclick="changeInfoTab('stats', 'main', 'evo-chain')" id="stats">Stats</span></div>
+                <div><span onclick="changeInfoTab('evo-chain', 'main', 'stats')" id="evo-chain">Evo-Chain</span></div>
+            </nav>
+            <div id="detailed-info-main">
+                <div>
+                    <span>Base Experience</span>
+                    <span>${baseExp} xp</span>
+                </div>
+                <div>
+                    <span>Height</span>
+                    <span>${height} m</span>
+                </div>
+                <div>
+                    <span>Weight</span>
+                    <span>${weight} kg</span>
+                </div>
+            </div>
+            <div id="detailed-info-stats" class="d-none">
+                <div class="stat-container">
+                    <span>HP</span>
+                    <div>
+                        <div class="visual-stat" style="width: ${hpInPercent}%;"></div>
+                    </div>
+                </div>
+                <div class="stat-container">
+                    <span>Attack</span>
+                    <div>
+                        <div class="visual-stat" style="width: ${attackInPercent}%;"></div>
+                    </div>
+                </div>
+                <div class="stat-container">
+                    <span>Defense</span>
+                    <div>
+                        <div class="visual-stat" style="width: ${defenseInPercent}%;"></div>
+                    </div>
+                </div>
+                <div class="stat-container">
+                    <span>Special Attack</span>
+                    <div>
+                        <div class="visual-stat" style="width: ${specialattackInPercent}%;"></div>
+                    </div>
+                </div>
+                <div class="stat-container">
+                    <span>Special Defense</span>
+                    <div>
+                        <div class="visual-stat" style="width: ${specialdefenseInPercent}%;"></div>
+                    </div>
+                </div>
+                <div class="stat-container">
+                    <span>Speed</span>
+                    <div>
+                        <div class="visual-stat" style="width: ${speedInPercent}%;"></div>
+                    </div>
+                </div>
+            </div>
+            <div id="detailed-info-evo-chain" class="d-none">
+                ${evoChain}
+            </div>
+            <div class="last-next-pokemon">
+                <button><i class="fa fa-arrow-left"></i></button>
+                <button><i class="fa fa-arrow-right"></i></button>
+            </div>
+    `
+}
+
+function getEvoChainImgTemplate(evoNr, pokemonId) {
+    return`
+            <img id="evo-img-${evoNr}"
+                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemonId}.png"
+                alt="">
+            <img id="evo-img-${evoNr}-shiny"
+                src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/${pokemonId}.png"
+                alt=""
+                class="d-none">
+    `
+}
+
+function getEvoChainArrowTemplate() {
+    return`
+            <i class="fa fa-angle-double-right"></i>
+    `
+}
+
+function getLoadSpinnerTemplate() {
+    return`
+            <div class="pokeball-container">
+                <svg class="pokeball" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="64" height="64">
+                    <circle cx="50" cy="50" r="48" fill="none" stroke="white" stroke-width="4" />
+                    <rect x="2" y="48" width="96" height="4" fill="white" />
+                    <circle cx="50" cy="50" r="12" fill="none" stroke="white" stroke-width="6" />
+                    <circle cx="50" cy="50" r="6" fill="white" />
+                </svg>
+            </div>
+    `
+}
+
+function getShowMoreButtonTemplate() {
+    return`
+            <button class="render-more-button">Show More</button>
+    `
+}
+
+{/* <img id="evo-img-1"
+src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png"
+alt="">
+<img id="evo-img-1-shiny"
+src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/4.png"
+alt=""
+class="d-none">
+<i class="fa fa-angle-double-right"></i>
+<img id="evo-img-2"
+src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/5.png"
+alt="">
+<img id="evo-img-2-shiny"
+src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/5.png"
+alt=""
+class="d-none">
+<i class="fa fa-angle-double-right"></i>
+<img id="evo-img-3"
+src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/6.png"
+alt="">
+<img id="evo-img-3-shiny"
+src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/shiny/6.png"
+alt=""
+class="d-none"> */}
